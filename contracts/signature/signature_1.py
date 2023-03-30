97c65f7830e3a8e4a0a9475546e858bd1c6c3475
Isabela Travaglia <isabelatravaglia@gmail.com>
2023-03-30 17:41:23 +0000

    increased transfer amount to the deployed contracts

--- contracts/signature/signature_1.py
+++ contracts/signature/signature_1.py
@@ -25,12 +25,12 @@ async def main():
     #
     # ACTION ITEM 1: find the first EIP number discussing account abstraction
     #
-    INPUT_1 = 0
+    INPUT_1 = 2938
 
     #
     # ACTION ITEM 2: find the second EIP number discussing account abstraction
     #
-    INPUT_2 = 0
+    INPUT_2 = 4337
     blue.print(f"First account abstraction EIP: \n\thttps://eips.ethereum.org/EIPS/eip-{INPUT_1}")
     blue.print(f"Second account abstraction EIP: \n\thttps://eips.ethereum.org/EIPS/eip-{INPUT_2}\n")
 
